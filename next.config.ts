16b8d239a16065b89dfc8500c58bfff822319c84
Aditya Pradita <pradita397@gmail.com>
2026-03-03 10:57:06 +0700

    Use next image for sprites to optimize performance

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        pathname: "/PokeAPI/sprites/**",
+      },
+    ],
+  },
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
